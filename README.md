1fba7879ef3ece2660570b7aa7c5e92fa28805c6
Connor Jacobs <connorjacobs222@gmail.com>
2021-02-22 21:15:24 -0500

    finalized java script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Password-Generator
+
+## Description
+
+The third homework assignment was to code the javascript for a password generator. The goal was to generate a random password based on a four pieces of criteria: uppercase letters, lowercase letters, special characters, and numbers. The password length was required to be more than 7 characters and less than 129 characters. 
+
+With the assistance of my tutor, I was able to begin the assignment by generating arrays to store the criteria. Then I moved to prompting the user to tell us how long they want their password to be, followed by prompts asking what they want their password criteria to be. I then stored the arrays into an object that was called in the next function. The next function generated an empty array that was filled in by for loops depending on whether or not the user chose the character. 
+
+## Screenshot 
+
+
+
--- script.js
+++ script.js
@@ -90,24 +90,98 @@ var specialCharacters = [
 
 // function for password prompts
 function passwordPrompts() {
-  var pwLength = parsInt(
+// changing user input from string to integer
+  var pwLength = parseInt(
     prompt("How many characters do you want your password to be?")
   );
 
-  if(pwLength < 8) {
-    alert("Password length is too short")
+  if (isNaN(pwLength) === true) {
+    alert("You must select a number");
+    return;
   }
+
+  // if they choose a password length too short, we give them an alert
+  if(pwLength < 8) {
+    alert("Password length is too short");
+    return;
+  }
+
+  // if user chooses a password length too long, we give them an alert
   if(pwLength > 128) {
-    alert("Password length is too long")
+    alert("Password length is too long");
+    return;
   }
   
+  // Using booleans to intake user's selections of password criteria
   var chooseLowercase = confirm("If you want lower case characters, click OK.");
   var chooseUppercase = confirm("If you want upper case characters, click OK.");
   var chooseNumerics = confirm("If you want numbers, click OK.");
   var chooseSpecialCharacters = confirm("If you want special characters, click OK.");
+
+  // if user selects cancel to every criteria option, we give them an alert 
+  if(
+    chooseLowercase === false &&
+    chooseUppercase === false &&
+    chooseNumerics === false &&
+    chooseSpecialCharacters === false) {
+    alert("You must select at least one character type");
+    return;
+  }
+
+
+  // creating an object to pull in user selections in the next function
+var options = {
+  length: pwLength,
+  chooseLowercase: chooseLowercase,
+  chooseUppercase: chooseUppercase, 
+  chooseNumerics: chooseNumerics, 
+  chooseSpecialCharacters: chooseSpecialCharacters,
+};
+
+return options;
 }
 
-passwordPrompts() 
+function createPassword() {
+  // calling previous function to access user inputs
+  var options = passwordPrompts();
+
+  // establishing an empty array to create our password 
+  var passwordEntry = [];
+
+  // if user chose lower case options, we are pushing that into the empty array
+  if (options.chooseLowercase) {
+    for (i = 0 ; i < lowerCase.length; i++) {
+      passwordEntry.push(lowerCase[i]);
+    }
+  }
+  // if user chose special charaters, we are pushing that into the empty array
+  if (options.chooseSpecialCharacters) {
+    for (i = 0 ; i < specialCharacters.length; i++) {
+      passwordEntry.push(specialCharacters[i]);
+    }
+  }
+  // if user chose numerics, we are pushing that into the empty array
+  if (options.chooseNumerics) {
+    for (i = 0 ; i < numerics.length; i++) {
+      passwordEntry.push(numerics[i]);
+    }
+  }
+  // if user chose upppercase, we are pushing that into the empty array 
+  if (options.chooseUppercase) {
+    for (i = 0 ; i < upperCase.length; i++) {
+      passwordEntry.push(upperCase[i]);
+    }
+  }
+ console.log(passwordEntry) 
+// creating a variable to produce password
+  var producePassword = ""
+// randomizing password
+  for (i = 0; i < options.length; i++) {
+    producePassword += passwordEntry[Math.floor(Math.random() * passwordEntry.length)];
+  }
+  console.log(producePassword)
+return producePassword;
+}
 
 
 // Assignment Code
@@ -115,7 +189,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = createPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -123,3 +197,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
